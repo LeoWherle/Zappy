5f9b5d9ac97e7875caaee432baf804af3ce219e8
Martin <martin.sabatier@gmail.com>
2024-05-23 14:02:35 +0200

    fix: changed the cpp bindings to a submoudle and changed install.sh and makefile

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ git submodule update --init --recursive
 cd gui/raylib/src
 make clean
 make PLATFORM=PLATFORM_DESKTOP
+cd ../../../
+cd gui/raylib_cpp
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
