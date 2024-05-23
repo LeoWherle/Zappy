c3d2af50cefddd7557f3c4e360f6297288e877ff
Martin <martin.sabatier@gmail.com>
2024-05-23 14:08:40 +0200

    fix: comments on install.sh and fixed .gitmodules

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+#Updates the modules if there are any changes
 git submodule update --init --recursive
+
+#Builds the raylib library
 cd gui/raylib/src
 make clean
 make PLATFORM=PLATFORM_DESKTOP
 cd ../../../
+
+#Builds the raylib_cpp bindings
 cd gui/raylib_cpp
 rm -rf build
 mkdir build
