57f72361fc8168fefaa8623687bc029df2ea4bc8
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-24 01:19:37 +0200

    edit: gource script

--- gource.sh
+++ gource.sh
@@ -1,1 +1,8 @@
-gource -r 60 -b 000000 --user-image-dir bonus/ressources/avatars --bloom-multiplier 0.7 --background-image bonus/ressources/darkstar.png --highlight-dirs --font-size 20 --font-file bonus/ressources/Starjedi.ttf --stop-at-end --hide progress,usernames,date --title "The segfault strikes back"
+#!/bin/bash
+
+RESSOURCES=bonus/gource
+
+gource -r 60 -b 000000 --user-image-dir $RESSOURCES/avatars \
+--bloom-multiplier 0.7 --background-image $RESSOURCES/background.png \
+--highlight-dirs --font-size 20 --font-file $RESSOURCES/font.ttf \
+--stop-at-end --hide progress,usernames,date --title "ZapPikmin"
