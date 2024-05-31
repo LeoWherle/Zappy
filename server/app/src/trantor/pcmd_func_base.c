d2e93080f5841a0058eae4d2e5676dff6c7c3ebb
mathematisse <lucien.pineau@epitech.eu>
2024-05-31 18:27:06 +0200

    add: spamming trantor

--- server/app/src/trantor/pcmd_func_base.c
+++ server/app/src/trantor/pcmd_func_base.c
@@ -14,20 +14,33 @@ void player_error(pcmd_args_t *args)
     SAY_KO(&args->player->response_buffer);
 }
 
+static void immediate_ppo(pcmd_args_t *args)
+{
+    talkf(args->log, "ppo %d %d %d %d\n", args->player->n,
+        args->player->coord[0], args->player->coord[1],
+        args->player->direction + 1);
+}
+
 void player_forward(pcmd_args_t *args)
 {
     SAY_OK(&args->player->response_buffer);
     player_move(args->player, args->map, args->player->direction);
+    if (args->spam_gui)
+        immediate_ppo(args);
 }
 
 void player_right(pcmd_args_t *args)
 {
     SAY_OK(&args->player->response_buffer);
     args->player->direction = (args->player->direction + 1) % 4;
+    if (args->spam_gui)
+        immediate_ppo(args);
 }
 
 void player_left(pcmd_args_t *args)
 {
     SAY_OK(&args->player->response_buffer);
     args->player->direction = (args->player->direction + 3) % 4;
+    if (args->spam_gui)
+        immediate_ppo(args);
 }
