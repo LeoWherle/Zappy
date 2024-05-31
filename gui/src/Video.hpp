a70cd1de5c072ab750f0d5027124d8b36f80d7b5
Leo Rentmeister <leo.wehrle@epitech.eu>
2024-05-31 21:29:28 +0200

    add: wip video playing class

--- gui/src/Video.hpp
+++ gui/src/Video.hpp
@@ -13,11 +13,13 @@
 
 #include "Image.hpp"
 #include "Texture.hpp"
+
+#define PL_MPEG_IMPLEMENTATION
 #include "pl_mpeg.h"
 
 class Video {
 private:
-    std::unique_ptr<plm_t> _plm;
+    plm_t *_plm;
     double _framerate;
     double _samplerate;
     raylib::Image _Imframe;
@@ -42,47 +44,49 @@ public:
 
     Video(const std::string &file_path, bool loop = false)
     {
-        _plm = std::make_unique<plm_t>(plm_create_with_filename(file_path.c_str()));
-        if (!_plm) {
+        _plm = plm_create_with_filename(file_path.c_str());
+        if (_plm == nullptr) {
             throw InvalidFile("Failed to open video file");
         }
-        plm_set_loop(_plm.get(), true);
-        _framerate = plm_get_framerate(_plm.get());
-        _samplerate = plm_get_samplerate(_plm.get());
-        _Imframe.width = plm_get_width(_plm.get());
-        _Imframe.height = plm_get_height(_plm.get());
+        plm_set_loop(_plm, true);
+        _framerate = plm_get_framerate(_plm);
+        _samplerate = plm_get_samplerate(_plm);
+        _Imframe.width = plm_get_width(_plm);
+        _Imframe.height = plm_get_height(_plm);
         _Imframe.mipmaps = 1;
         _Imframe.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
         _Imframe.data = RL_MALLOC(_Imframe.width * _Imframe.height * 3);
-        plm_set_loop(_plm.get(), loop);
+        plm_set_loop(_plm, loop);
     }
 
     Video(const char *file_path, bool loop = false)
     {
-        _plm = std::make_unique<plm_t>(plm_create_with_filename(file_path));
-        if (!_plm) {
+        _plm = plm_create_with_filename(file_path);
+        if (_plm == nullptr) {
             throw InvalidFile("Failed to open video file");
         }
-        plm_set_loop(_plm.get(), true);
-        _framerate = plm_get_framerate(_plm.get());
-        _samplerate = plm_get_samplerate(_plm.get());
-        _Imframe.width = plm_get_width(_plm.get());
-        _Imframe.height = plm_get_height(_plm.get());
+        plm_set_loop(_plm, true);
+        _framerate = plm_get_framerate(_plm);
+        _samplerate = plm_get_samplerate(_plm);
+        _Imframe.width = plm_get_width(_plm);
+        _Imframe.height = plm_get_height(_plm);
         _Imframe.mipmaps = 1;
         _Imframe.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
         _Imframe.data = RL_MALLOC(_Imframe.width * _Imframe.height * 3);
-        plm_set_loop(_plm.get(), loop);
+        plm_set_loop(_plm, loop);
     }
 
     void Resume(void) { _pause = false; }
-
     void Pause(void) { _pause = true; }
+    void setRunning(bool running) { _pause = !running; }
 
     void Loop(bool loop)
     {
         _ended = false;
-        plm_set_loop(_plm.get(), loop);
+        plm_set_loop(_plm, loop);
     }
+
+    bool isLooping(void) const { return _loop; }
 
     bool isEnded(void) const { return _ended; }
 
@@ -91,10 +95,10 @@ public:
     void Rewind(void)
     {
         _ended = false;
-        bool old = plm_get_loop(_plm.get());
-        plm_set_loop(_plm.get(), true);
-        plm_rewind(_plm.get());
-        plm_set_loop(_plm.get(), old);
+        bool old = plm_get_loop(_plm);
+        plm_set_loop(_plm, true);
+        plm_rewind(_plm);
+        plm_set_loop(_plm, old);
     }
 
     void Update()
@@ -104,13 +108,13 @@ public:
         if (_pause || _ended)
             return;
         double time = (GetTime() - _base_time);
-        if (_base_time >= 1.0 / _framerate) {
+        if (time >= 1.0 / _framerate) {
             _base_time = GetTime();
-            if (plm_has_ended(_plm.get())) {
-                plm_rewind(_plm.get());
+            if (plm_has_ended(_plm)) {
+                plm_rewind(_plm);
             }
             // Decode video frame
-            frame = plm_decode_video(_plm.get());
+            frame = plm_decode_video(_plm);
             if (frame) {
                 plm_frame_to_rgb(frame, (uint8_t *) _Imframe.data, _Imframe.width * 3);
                 UpdateTexture(_texture, _Imframe.data);
@@ -118,18 +122,29 @@ public:
                 memset(_Imframe.data, 0, _Imframe.width * _Imframe.height * 3);
                 UpdateTexture(_texture, _Imframe.data);
             }
-            if (plm_has_ended(_plm.get())) {
+            if (plm_has_ended(_plm)) {
                 if (_loop) {
-                    plm_rewind(_plm.get());
+                    plm_rewind(_plm);
                 }
             }
         }
     }
 
+    // Center the video on the screen
+    void Draw(Color color = WHITE)
+    {
+        DrawTexture(
+            _texture, GetScreenWidth() / 2 - _texture.width / 2, GetScreenHeight() / 2 - _texture.height / 2,
+            color
+        );
+    }
+
+    void Draw(int x, int y, Color color = WHITE) { DrawTexture(_texture, x, y, color); }
+
     ~Video()
     {
-        if (_plm) {
-            plm_destroy(_plm.get());
+        if (_plm != nullptr) {
+            plm_destroy(_plm);
         }
     }
 };
