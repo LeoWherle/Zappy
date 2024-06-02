d9169ffd0aaf00658b1707e2526d933f40f0840b
Martin Sabatier <martin.sabatier@gmail.com>
2024-06-02 16:40:39 +0200

    add: static model bank

--- gui/src/ModelBank.cpp
+++ gui/src/ModelBank.cpp
@@ -6,32 +6,60 @@
 */
 
 #include "ModelBank.hpp"
+#include "ModelAnimation.hpp"
+#include "raylib.h"
+#include <iostream>
 
 // model name | <model file | texture file>
-static const std::map<std::string, std::pair<std::string, std::string>> nameFile({
-});
+const std::map<std::string, ModelInfo> ModelBank::modelInfo = {
+    {"RedPikmin", {"gui/res/models/RedPikmin.iqm", "gui/res/textures/RedPikmin.png"}},
+    {"FlowerTop", {"gui/res/models/FlowerTop.iqm", "gui/res/textures/FlowerTop.png"}},
+    {"BudTop", {"gui/res/models/BudTop.iqm", "gui/res/textures/BudTop.png"}},
+    {"LeafTop", {"gui/res/models/LeafTop.iqm", "gui/res/textures/LeafTop.png"}},
+};
 
-ModelBank::ModelBank(void)
-{
-}
+std::map<std::string, FullModel_t> ModelBank::models;
 
-ModelBank::~ModelBank(void)
+FullModel_t *ModelBank::get(std::string ressourceName)
 {
-    for (auto &[key, value] : _models) {
-        UnloadModel(value.model);
-        UnloadTexture(value.texture);
-    }
-}
-
-raylib::Model *ModelBank::get(std::string ressourceName)
-{
-    if (_models.find(ressourceName) == _models.end()) {
-        if (nameFile.find(ressourceName) == nameFile.end()) {
+    if (models.find(ressourceName) == models.end()) {
+        if (modelInfo.find(ressourceName) == modelInfo.end()) {
             return nullptr;
             //throw ModelBank::InvalidModel(ressourceName);
         }
-        _models[ressourceName] = {raylib::Model(nameFile.at(ressourceName).first), raylib::Texture2D(nameFile.at(ressourceName).first)};
-        SetMaterialTexture(&_models[ressourceName].model.materials[0], MATERIAL_MAP_DIFFUSE, _models[ressourceName].texture);
+        std::string modelPath = modelInfo.at(ressourceName).modelPath;
+        std::string texturePath = modelInfo.at(ressourceName).texturePath;
+        models[ressourceName] = FullModel_t{
+            raylib::Model(modelPath.c_str()),
+            raylib::Texture(texturePath.c_str()),
+            raylib::ModelAnimation::Load(modelPath.c_str())
+            };
+        SetMaterialTexture(&models[ressourceName].model.materials[0], MATERIAL_MAP_DIFFUSE, models[ressourceName].texture);
     }
-    return &_models[ressourceName].model;
+    return &models[ressourceName];
 }
+
+// ModelBank::ModelBank(void)
+// {
+// }
+
+// ModelBank::~ModelBank(void)
+// {
+//     for (auto &[key, value] : _models) {
+//         UnloadModel(value.model);
+//         UnloadTexture(value.texture);
+//     }
+// }
+
+// raylib::Model *ModelBank::get(std::string ressourceName)
+// {
+//     if (_models.find(ressourceName) == _models.end()) {
+//         if (nameFile.find(ressourceName) == nameFile.end()) {
+//             return nullptr;
+//             //throw ModelBank::InvalidModel(ressourceName);
+//         }
+//         _models[ressourceName] = {raylib::Model(nameFile.at(ressourceName).first), raylib::Texture2D(nameFile.at(ressourceName).first)};
+//         SetMaterialTexture(&_models[ressourceName].model.materials[0], MATERIAL_MAP_DIFFUSE, _models[ressourceName].texture);
+//     }
+//     return &_models[ressourceName].model;
+// }
--- gui/src/ModelBank.hpp
+++ gui/src/ModelBank.hpp
@@ -7,38 +7,40 @@
 
 #pragma once
 
+#include "ModelAnimation.hpp"
 #include <raylib-cpp.hpp>
 #include <map>
+#include <vector>
+
+typedef struct ModelInfo {
+    std::string modelPath;
+    std::string texturePath;
+} ModelInfo_t;
+
+typedef struct FullModel {
+    raylib::Model model;
+    raylib::Texture2D texture;
+    std::vector<raylib::ModelAnimation> animations;
+} FullModel_t;
 
 class ModelBank
 {
-public:
-    ModelBank(void);
-    ~ModelBank(void);
-
-    raylib::Model *get(std::string ressourceName);
-
-    struct texturedModel {
-        raylib::Model model;
-        raylib::Texture2D texture;
-    };
-
-    class InvalidModel : public std::exception
-    {
     public:
-        InvalidModel(std::string name) : _str("invalid model name " + name)
-        {}
 
-        const char* what() const noexcept override
+        static const std::map<std::string, ModelInfo_t> modelInfo;
+        static std::map<std::string, FullModel_t> models;
+
+        static FullModel_t *get(std::string ressourceName);
+
+        class InvalidModel : public std::exception
         {
-            return _str.c_str();
-        }
+            public:
+                InvalidModel(std::string name) : _str("invalid model name " + name) {}
 
-    private:
-        std::string _str;
-    };
+                const char* what() const noexcept override { return _str.c_str(); }
 
-private:
-    std::map<std::string, texturedModel> _models;
-    raylib::Model _default;
+            private:
+                std::string _str;
+        };
+
 };
