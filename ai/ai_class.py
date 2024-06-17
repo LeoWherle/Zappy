8d6627a2acfc670df0d4a636891d61f6800a3f13
TheodoreEpitech <theodore.magna@epitech.eu>
2024-06-17 16:38:10 +0200

    add: more security on AI with our gui

--- ai/ai_class.py
+++ ai/ai_class.py
@@ -34,9 +34,9 @@ class AI:
             self.dead = True
             return
         self.net.logger.info(f"Team {team} has {team_slots_left} slots left", self.id)
-        self.needed_food = self.net.map_x + 15
-        if self.needed_food < 25:
-            self.needed_food = 25
+        self.needed_food = self.net.map_x + 20
+        if self.needed_food < 30:
+            self.needed_food = 30
 
     #---------------------------------#
     #           Send and read         #
