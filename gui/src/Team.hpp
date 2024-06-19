555630ec09c7df298872f8213b0fe409d1f02699
Nathanael <nathanael.epitech@gmail.com>
2024-06-19 10:59:46 +0200

    add: team object

---
+++ gui/src/Team.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** zappy
+** File description:
+** Pikmin
+*/
+
+#include "ModelBank.hpp"
+
+#pragma once
+
+namespace GUI {
+    class Team
+    {
+    public:
+        Team(const std::string &name, const std::shared_ptr<GuiModel> &model, const raylib::Color &color) : _name(name), _model(model), _color(color)
+        {}
+
+        std::string &getName(void) { return _name; }
+        std::shared_ptr<GuiModel> &getModel(void) { return _model; }
+        raylib::Color &getColor(void) { return _color; }
+
+    private:
+        std::string _name;
+        std::shared_ptr<GuiModel> _model;
+        raylib::Color _color;
+    };
+}
