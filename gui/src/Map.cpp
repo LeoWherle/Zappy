22b49869b5133d18dd86f9bfd798e26c314eda69
Martin Sabatier <martin.sabatier@gmail.com>
2024-06-23 21:37:54 +0200

    add: kitchen to map with position switch

--- gui/src/Map.cpp
+++ gui/src/Map.cpp
@@ -7,18 +7,25 @@
 
 #include "Map.hpp"
 #include "ModelBank.hpp"
-#include <iostream>
+#include "Vector3.hpp"
+#include <vector>
 
 namespace GUI {
+
+    static const std::vector<raylib::Vector3> mapPositions = {
+        raylib::Vector3(-1.8f, -4.0f, 7.9f),
+        raylib::Vector3(11.66f, -10.0f, -31.68f),
+    };
+
     Map::Map()
     {
         _furniture = nullptr;
         _walls = nullptr;
         _rug = nullptr;
+        _currentPos = 0;
         _scale = 0.05f;
         _rotation = 90;
-        _position = raylib::Vector3(-1.1f, -2.4f, 4.75f);
-        //_position = raylib::Vector3(0.0f, 0.0f, 0.0f);
+        _position = mapPositions[0];
         _rotationAxis = raylib::Vector3(1.0f, 0.0f, 0.0f);
     }
 
@@ -31,6 +38,8 @@ namespace GUI {
         _furniture = ModelBank::get(ModelType::MAP);
         _walls = ModelBank::get(ModelType::WALLS);
         _rug = ModelBank::get(ModelType::RUG);
+        _bigSofa = ModelBank::get(ModelType::BIGSOFA);
+        _sofa = ModelBank::get(ModelType::SOFA);
     }
 
     void Map::draw()
@@ -38,11 +47,21 @@ namespace GUI {
         _furniture->Draw(_position, _rotationAxis, _rotation, _scale, raylib::Color::White());
         _walls->Draw(_position, _rotationAxis, _rotation, _scale, raylib::Color::White());
         _rug->Draw(_position, _rotationAxis, _rotation, _scale, raylib::Color::White());
+        _bigSofa->Draw(_position, _rotationAxis, _rotation, _scale, raylib::Color::White());
+        _sofa->Draw(_position, _rotationAxis, _rotation, _scale, raylib::Color::White());
     }
 
     void Map::setScaling(float scale)
     {
         _scale *= scale;
         _position *= scale;
+    }
+
+    void Map::changePosition()
+    {
+        _currentPos += 1;
+        if (_currentPos >= mapPositions.size())
+            _currentPos = 0;
+        _position = mapPositions[_currentPos];
     }
 }
--- gui/src/Map.hpp
+++ gui/src/Map.hpp
@@ -20,15 +20,19 @@ namespace GUI {
             void draw();
             void setupModels();
             void setScaling(float scale);
+            void changePosition();
+            raylib::Vector3 _position;
 
         protected:
         private:
             float _scale;
             float _rotation;
-            raylib::Vector3 _position;
+            int _currentPos;
             raylib::Vector3 _rotationAxis;
             std::shared_ptr<GuiModel> _furniture;
             std::shared_ptr<GuiModel> _walls;
             std::shared_ptr<GuiModel> _rug;
+            std::shared_ptr<GuiModel> _bigSofa;
+            std::shared_ptr<GuiModel> _sofa;
     };
 }
